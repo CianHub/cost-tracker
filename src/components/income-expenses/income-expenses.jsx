38e2396c8fbe253b396c9172741786284ab3a70a
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-06 21:06:20 +0100

    add income and expenses with context

--- src/components/income-expenses/income-expenses.jsx
+++ src/components/income-expenses/income-expenses.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext)
+
+    const income = transactions.map((transaction) => transaction > 0)
+    const expenses = transactions.map((transaction) => transaction < 0)
+
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">€0.00</p>
+                <p className="money plus">€{income}</p>
             </div>
             <div>
-                <h4>Expense</h4>
-                <p className="money minus">-€0.00</p>
+                <h4>Expenses</h4>
+                <p className="money minus">-€{expenses}</p>
             </div>
         </div>
     )
