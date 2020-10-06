0cdf8de3b22eb951aff4f5e43304d5197130ef69
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-06 20:34:22 +0100

    Add skeleton, add basic GlobalState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/header/header';
+import { Balance } from './components/balance/balance';
 
+import Container from '@material-ui/core/Container';
+import { IncomeExpenses } from './components/income-expenses/income-expenses';
+import { TransactionList } from './components/TransactionList/TransactionList';
+import { AddTransaction } from './components/AddTransaction/AddTransaction';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Container>
+        <Header></Header>
+        <Balance></Balance>
+        <IncomeExpenses></IncomeExpenses>
+        <TransactionList></TransactionList>
+        <AddTransaction></AddTransaction>
+      </Container>
     </div>
   );
 }
---
+++ src/context/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useReducer } from 'react'
+
+const initialState = {
+    transactions: [
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+// Starting app context
+// Adds .Provider
+export const GlobalContext = createContext(initialState)
+
+// Provides the context to the app
+// Wraps the app
+export const GlobalProvider = () => ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    // Adds value prop
+    //
+    return <GlobalContext.Provider value={{ transactions: state.transactions }}>{children}</GlobalContext.Provider>
+}
