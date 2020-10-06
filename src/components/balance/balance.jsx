48381a17fb19a446146eaf52d3928b246c30a2e5
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-06 21:03:34 +0100

    Add total

--- src/components/balance/balance.jsx
+++ src/components/balance/balance.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext)
+
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
     return (
         <div>
-            Balance
+            <h4>Your Balance</h4>
+            <h1 id="balance">€{total}</h1>
         </div>
     )
 }
